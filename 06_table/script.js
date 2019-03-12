fb4023263fb854547f170319b0a901f16cf1520a
Koen Griffioen <koen.griffioen@capgemini.com>
2019-03-12 20:53:02 +0100

    Initial commit

---
+++ 06_table/script.js
@@ -0,0 +1,73 @@
+//////// Data generated with                ////////////
+//////// https://www.json-generator.com/    //////
+// [
+//   '{{repeat(5, 7)}}',
+//   {
+//     index: '{{index()}}',
+//     age: '{{integer(20, 40)}}',
+//     name: '{{firstName()}} {{surname()}}',
+//     email: '{{email()}}',
+//     phone: '+1 {{phone()}}',
+//     address: '{{integer(100, 999)}} {{street()}}, {{city()}}, {{state()}}, {{integer(100, 10000)}}',
+//     registered: '{{date(new Date(2014, 0, 1), new Date(), "YYYY-MM-ddThh:mm:ss Z")}}'
+//   }
+// ]
+
+const data = [
+    {
+        index: 0,
+        age: 40,
+        name: 'Hines Hayes',
+        email: 'hineshayes@xerex.com',
+        phone: '+1 (903) 525-3790',
+        address: '684 Woodrow Court, Ribera, Alabama, 7907',
+        registered: '2017-04-09T10:10:25 -02:00'
+    },
+    {
+        index: 1,
+        age: 37,
+        name: 'Rebekah Crane',
+        email: 'rebekahcrane@xerex.com',
+        phone: '+1 (967) 494-2007',
+        address: '723 Cameron Court, Emory, New Mexico, 4999',
+        registered: '2017-08-25T03:36:40 -02:00'
+    },
+    {
+        index: 2,
+        age: 37,
+        name: 'Helen Kim',
+        email: 'helenkim@xerex.com',
+        phone: '+1 (908) 486-2764',
+        address: '369 Gelston Avenue, Coldiron, Idaho, 7732',
+        registered: '2015-07-02T10:58:07 -02:00'
+    },
+    {
+        index: 3,
+        age: 24,
+        name: 'Earnestine Douglas',
+        email: 'earnestinedouglas@xerex.com',
+        phone: '+1 (876) 439-2005',
+        address: '635 Wogan Terrace, Deltaville, Nebraska, 3139',
+        registered: '2015-05-19T06:36:03 -02:00'
+    },
+    {
+        index: 4,
+        age: 36,
+        name: 'Madeleine Mullins',
+        email: 'madeleinemullins@xerex.com',
+        phone: '+1 (861) 551-2633',
+        address: '344 Schaefer Street, Finzel, New Jersey, 5101',
+        registered: '2015-08-24T09:57:44 -02:00'
+    },
+    {
+        index: 5,
+        age: 20,
+        name: 'Bridgett Carter',
+        email: 'bridgettcarter@xerex.com',
+        phone: '+1 (905) 445-2513',
+        address: '702 Vandervoort Avenue, Gwynn, Virgin Islands, 762',
+        registered: '2018-09-14T10:39:12 -02:00'
+    }
+];
+
+// Fill table with data
